69dc78e2822ac5e26edf3b6d376a8c4e8e377abf
Вадим Воробьев <vadimvorobev@Ruby.local>
2022-12-14 19:07:25 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -6,44 +6,47 @@
 class PingPong
 {
 public:
-	static constexpr std::size_t MAX = 3;
+    static constexpr std::size_t MAX = 3;
 
-	void ping()
-	{
-    	std::unique_lock<std::mutex> lock(m_);
-    	while (count_.load() < MAX)
-    	{
-        	std::cout << "Ping" << std::endl;
-        	count_++;
-        	cv_.notify_all();
-        	cv_.wait(lock);
-    	}
- 	}
+    void ping()
+    {
+        std::unique_lock<std::mutex> lock(m_);
+        while (count_.load() < MAX)
+        {
+            std::cout << "Ping" << std::endl;
+            count_++;
+            cv_.notify_all();
+            cv_.wait(lock);
+//            std::cout << "*" << std::endl;
+        }
+     }
 
-	void pong()
-	{
-    	std::unique_lock<std::mutex> lock(m_);
-    	while (count_.load() < MAX)
-    	{
-        	std::cout << "Pong" << std::endl;
-        	count_++;
-        	cv_.notify_all();
-        	cv_.wait(lock);
-    	}
-	}
+    void pong()
+    {
+        std::unique_lock<std::mutex> lock(m_);
+        while (count_.load() < MAX)
+        {
+            std::cout << "Pong" << std::endl;
+            count_++;
+            cv_.notify_all();
+            cv_.wait(lock);
+//            std::cout << "**" << std::endl;
+        }
+        cv_.notify_all();
+    }
 
 private:
-	std::atomic<std::size_t> count_;
-	std::mutex m_;
-	std::condition_variable cv_;
+    std::atomic<std::size_t> count_ = 0;
+    std::mutex m_;
+    std::condition_variable cv_;
 };
 
 int main()
 {
-	PingPong p;
-	std::thread pingThread(&PingPong::ping, &p);
-	std::thread pongThread(&PingPong::pong, &p);
+    PingPong p;
+    std::thread pingThread(&PingPong::ping, &p);
+    std::thread pongThread(&PingPong::pong, &p);
 
-	pingThread.join();
-	pongThread.join();
+    pingThread.join();
+    pongThread.join();
 }
